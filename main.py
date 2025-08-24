35f949d10667f75f6f5ac5fc170c9e74634bea99
Ron <proskille16@gmail.com>
2025-08-24 12:09:17 +0300

    added sprite groups

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from settings import *
+from pytmx.util_pygame import load_pygame
+from os.path import join
+from sprites import Sprite
 
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption("Miflazot")
+        pygame.display.set_caption('Miflazot')
+
+        # Groups
+        self.all_sprites = pygame.sprite.Group()
+
+        self.import_assets()
+        self.setup(self.tmx_maps['world'], 'house')
+
+    def import_assets(self):
+        self.tmx_maps = {'world' : load_pygame(join('..', 'data', 'maps', 'world.tmx'))}
+
+    def setup(self, tmx_map, player_start_pos):
+        for x, y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
 
     def run(self):
         while True:
@@ -15,8 +31,9 @@ class Game:
                     exit()
 
             # game logic
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     game = Game()
     game.run()
